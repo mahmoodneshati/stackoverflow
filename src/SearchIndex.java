c753ded76513390b88dbb2e36b3e85df1e0e0ee9
Zohreh <z.fallahnejad@gmail.com>
2015-08-21 22:02:24 +0430

    Search Classes Updated

--- src/SearchIndex.java
+++ src/SearchIndex.java
@@ -1,40 +1,26 @@
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.queryparser.classic.ParseException;
+/**
+ * Created by Zohreh on 08/20/2015.
+ */
+
+import org.apache.lucene.index.*;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.*;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.store.Directory;
+import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.search.ScoreDoc;
-import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Date;
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 import org.apache.lucene.document.*;
-
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class SearchIndex {
-
     public static void main(String[] args) {
         long start = new Date().getTime();
 
@@ -47,74 +33,176 @@ public class SearchIndex {
     public SearchIndex() {
         try {
             String indexDir = "index";
-        String q = "";
+            String q = "";
 
-        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexDir)));
-        IndexSearcher searcher = new IndexSearcher(reader);
-        Analyzer analyzer = new StandardAnalyzer();
-        String field="Id";
-        QueryParser parser = new QueryParser(field, analyzer);
+            IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexDir)));
+            IndexSearcher searcher = new IndexSearcher(reader);
+            Analyzer analyzer = new StandardAnalyzer();
+            String field="Id";
+            QueryParser parser = new QueryParser(field, analyzer);
 
-        String line;
-        int count=0;
-        BufferedReader reader2 = new BufferedReader(new FileReader("PostsN.xml"));
-        while ((line = reader2.readLine()) != null) {
-            if (line.trim().startsWith("<row")) {
-                count +=1;
-                if (count>1)
-                    break;
-                System.out.println(line);
-                Elements row = Jsoup.parse(line).getElementsByTag("row");
+            String line;
+            int count=0;
+            Date date=new Date();
+            Date date1=new Date();
+            BufferedReader breader = new BufferedReader(new FileReader("Posts1.xml"));
+            while ((line = breader.readLine()) != null) {
+                if (line.trim().startsWith("<row")) {
+                    count +=1;
+                    //if (count>1)
+                    //    break;
+                    System.out.println(line);
+                    Elements row = Jsoup.parse(line).getElementsByTag("row");
 
-                Integer Id = getIntegerValue(row, "Id");
-                Integer PostTypeId = getIntegerValue(row, "PostTypeId");
-                Integer ParentId = getIntegerValue(row, "ParentId");
-                Integer AcceptedAnswerId = getIntegerValue(row, "AcceptedAnswerId");
-                Date CreationDate = getDateValue(row, "CreationDate");
-                Integer Score = getIntegerValue(row, "Score");
-                Integer ViewCount = getIntegerValue(row, "ViewCount");
-                String Body = getStringValue(row, "Body");
-                Integer OwnerUserId = getIntegerValue(row, "OwnerUserId");
-                Integer LastEditorUserId = getIntegerValue(row, "LastEditorUserId");
-                String LastEditorDisplayName = getStringValue(row, "LastEditorDisplayName");
-                Date LastEditDate = getDateValue(row, "LastEditDate");
-                Date LastActivityDate = getDateValue(row, "LastActivityDate");
-                String Title = getStringValue(row, "Title");
-                ArrayList<String> Tags = getStringList(row, "Tags");
-                Integer AnswerCount = getIntegerValue(row, "AnswerCount");
-                Integer CommentCount = getIntegerValue(row, "CommentCount");
-                Integer FavoriteCount = getIntegerValue(row, "FavoriteCount");
-                Date CommunityOwnedDate = getDateValue(row, "CommunityOwnedDate");
+                    Integer PId = getIntegerValue(row, "Id");
 
-                Query query = parser.parse("4");
-                System.out.println("Searching for: " + query.toString(field));
+                    Query query = NumericRangeQuery.newIntRange("Id", PId, PId,true,true);
+                    //System.out.println("Searching for: " + query.toString(field));
 
-                // Collect enough docs to show 5 pages
-                TopDocs results = searcher.search(query, Integer.MAX_VALUE);
-                ScoreDoc[] hits = results.scoreDocs;
+                    ScoreDoc[] hits = searcher.search(query,Integer.MAX_VALUE).scoreDocs;
+                    //System.out.println("Found " + hits.length + " hits.");
+                    for (int i = 0; i < hits.length; ++i) {
+                        int docId = hits[i].doc;
+                        Document d = searcher.doc(docId);
+                        System.out.println(d.toString());
+                        List<IndexableField> fields= d.getFields();
+                        //System.out.println(FieldName.name()+" "+FieldName.stringValue());
+                        for (IndexableField FieldName : fields)
+                            switch (FieldName.name()) {
+                                case "PostTypeId":
+                                    if (((getIntegerValue(row, "PostTypeId") != null) && (getIntegerValue(row, "PostTypeId") != Integer.parseInt(d.get("PostTypeId")))) || ((getIntegerValue(row, "PostTypeId") == null && Integer.parseInt(d.get("PostTypeId")) != -1)))
+                                        System.out.println("Error in PostTypeId Field");
+                                    break;
+                                case "ParentId":
+                                    if ((getIntegerValue(row, "ParentId") != null) && (getIntegerValue(row, "ParentId") != Integer.parseInt(d.get("ParentId"))) || ((getIntegerValue(row, "ParentId") == null && Integer.parseInt(d.get("ParentId")) != -1)))
+                                        System.out.println("Error in ParentId Field");
+                                    break;
+                                case "AcceptedAnswerId":
+                                    if ((getIntegerValue(row, "AcceptedAnswerId") != null) && (getIntegerValue(row, "AcceptedAnswerId") != Integer.parseInt(d.get("AcceptedAnswerId"))) || ((getIntegerValue(row, "AcceptedAnswerId") == null && Integer.parseInt(d.get("AcceptedAnswerId")) != -1)))
+                                        System.out.println("Error in AcceptedAnswerId Field");
+                                    break;
+                                case "Score":
+                                    if ((getIntegerValue(row, "Score") != null) && (getIntegerValue(row, "Score") != Integer.parseInt(d.get("Score"))) || ((getIntegerValue(row, "Score") == null && Integer.parseInt(d.get("Score")) != -1)))
+                                        System.out.println("Error in Score Field");
+                                    break;
+                                case "ViewCount":
+                                    if ((getIntegerValue(row, "ViewCount") != null) && (getIntegerValue(row, "ViewCount") != Integer.parseInt(d.get("ViewCount"))) || ((getIntegerValue(row, "ViewCount") == null && Integer.parseInt(d.get("ViewCount")) != -1)))
+                                        System.out.println("Error in ViewCount Field");
+                                    break;
+                                case "OwnerUserId":
+                                    if ((getIntegerValue(row, "OwnerUserId") != null) && (getIntegerValue(row, "OwnerUserId") != Integer.parseInt(d.get("OwnerUserId"))) || ((getIntegerValue(row, "OwnerUserId") == null && Integer.parseInt(d.get("OwnerUserId")) != -1)))
+                                        System.out.println("Error in OwnerUserId Field");
+                                    break;
+                                case "LastEditorUserId":
+                                    if ((getIntegerValue(row, "LastEditorUserId") != null) && (getIntegerValue(row, "LastEditorUserId") != Integer.parseInt(d.get("LastEditorUserId"))) || ((getIntegerValue(row, "LastEditorUserId") == null && Integer.parseInt(d.get("LastEditorUserId")) != -1)))
+                                        System.out.println("Error in LastEditorUserId Field");
+                                    break;
+                                case "AnswerCount":
+                                    if ((getIntegerValue(row, "AnswerCount") != null) && (getIntegerValue(row, "AnswerCount") != Integer.parseInt(d.get("AnswerCount"))) || ((getIntegerValue(row, "AnswerCount") == null && Integer.parseInt(d.get("AnswerCount")) != -1)))
+                                        System.out.println("Error in AnswerCount Field");
+                                    break;
+                                case "CommentCount":
+                                    if ((getIntegerValue(row, "CommentCount") != null) && (getIntegerValue(row, "CommentCount") != Integer.parseInt(d.get("CommentCount"))) || ((getIntegerValue(row, "CommentCount") == null && Integer.parseInt(d.get("CommentCount")) != -1)))
+                                        System.out.println("Error in CommentCount Field");
+                                    break;
+                                case "FavoriteCount":
+                                    if ((getIntegerValue(row, "FavoriteCount") != null) && (getIntegerValue(row, "FavoriteCount") != Integer.parseInt(d.get("FavoriteCount"))) || ((getIntegerValue(row, "FavoriteCount") == null && Integer.parseInt(d.get("FavoriteCount")) != -1)))
+                                        System.out.println("Error in FavoriteCount Field");
+                                    break;
+                                case "LastEditorDisplayName":
+                                    if (!(getStringValue(row, "LastEditorDisplayName").equalsIgnoreCase(d.get("LastEditorDisplayName"))))
+                                        System.out.println("Error in LastEditorDisplayName Field");
+                                    break;
+                                case "Tags":
+                                    if (!(row.attr("Tags").equalsIgnoreCase(d.get("Tags"))))
+                                        System.out.println("Error in Tags Field");
+                                    break;
+                                case "CreationDate":
+                                    date = getDateValue(row, "CreationDate");
+                                    if (date==null)
+                                        break;
+                                    //System.out.println("XML: " + date.toString());
+                                    date1 = DateTools.stringToDate(d.get("CreationDate"));
+                                    //System.out.println("Doc: " + date1.toString());
+                                    if(date.equals(date1)){
+                                        System.out.println("XML Date is equal Doc Date in CreationDate Field");
+                                    }else if(date.compareTo(date1)>0){
+                                        System.out.println("XML Date is after Doc Date in CreationDate Field");
+                                    }else if(date.compareTo(date1)<0){
+                                        System.out.println("XML Date is before Doc Date in CreationDate Field");
+                                    }
+                                    break;
+                                case "LastEditDate":
+                                    date = getDateValue(row, "LastEditDate");
+                                    if (date==null)
+                                        break;
+                                    //System.out.println("XML: " + date.toString());
+                                    date1 = DateTools.stringToDate(d.get("LastEditDate"));
+                                    //System.out.println("Doc: " + date1.toString());
+                                    if(date.equals(date1)){
+                                        System.out.println("XML Date is equal Doc Date in LastEditDate Field");
+                                    }else if(date.compareTo(date1)>0){
+                                        System.out.println("XML Date is after Doc Date in LastEditDate Field");
+                                    }else if(date.compareTo(date1)<0){
+                                        System.out.println("XML Date is before Doc Date in LastEditDate Field");
+                                    }
+                                    break;
+                                case "LastActivityDate":
+                                    date = getDateValue(row, "LastActivityDate");
+                                    if (date==null)
+                                        break;
+                                    //System.out.println("XML: " + date.toString());
+                                    date1= DateTools.stringToDate(d.get("LastActivityDate"));
+                                    //System.out.println("Doc: " + date1.toString());
+                                    if(date.equals(date1)){
+                                        System.out.println("XML Date is equal Doc Date in LastActivityDate Field");
+                                    }else if(date.compareTo(date1)>0){
+                                        System.out.println("XML Date is after Doc Date in LastActivityDate Field");
+                                    }else if(date.compareTo(date1)<0){
+                                        System.out.println("XML Date is before Doc Date in LastActivityDate Field");
+                                    }
+                                    break;
+                                case "CommunityOwnedDate":
+                                    date = getDateValue(row, "CommunityOwnedDate");
+                                    if (date==null)
+                                        break;
+                                    //System.out.println("XML: " + date.toString());
+                                    date1= DateTools.stringToDate(d.get("CommunityOwnedDate"));
+                                    //System.out.println("Doc: " + date1.toString());
+                                    if(date.equals(date1)){
+                                        System.out.println("XML Date is equal Doc Date in CommunityOwnedDate Field");
+                                    }else if(date.compareTo(date1)>0){
+                                        System.out.println("XML Date is after Doc Date in CommunityOwnedDate Field");
+                                    }else if(date.compareTo(date1)<0){
+                                        System.out.println("XML Date is before Doc Date in CommunityOwnedDate Field");
+                                    }
+                                    break;
+                                default:
+                                    //System.out.println("Another field name!: "+FieldName.name());
+                                    break;
+                            }
 
-                int numTotalHits = results.totalHits;
-                System.out.println(numTotalHits + " total matching documents");
 
-                /*for (int i = 0; i < hits.length(); i++) {
-                    Document doc = hits.doc(i);
-                    System.out.println(doc.get("filename"));
-                }*/
+                    }
+                }
             }
-        }
-        } catch (IOException | ParseException e) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (java.text.ParseException e) {
             e.printStackTrace();
         }
     }
 
-    private ArrayList<String> getStringList(Elements row, String tag) {
-        ArrayList<String> out = new ArrayList<>();
-        String[] ss = row.attr(tag).split("&lt;|&gt;");
-        for (String s : ss) {
-            if (s != null && s.trim().length() > 0)
-                out.add(s.trim());
+    private Integer getIntegerValue(Elements row, String tag) {
+
+        Integer value;
+        try {
+            value = Integer.parseInt(row.attr(tag));
+        } catch (Exception e) {
+            value = null;
         }
-        return out;
+        return value;
 
     }
 
@@ -141,16 +229,4 @@ public class SearchIndex {
         return date;
     }
 
-    private Integer getIntegerValue(Elements row, String tag) {
-
-        Integer value;
-        try {
-            value = Integer.parseInt(row.attr(tag));
-        } catch (Exception e) {
-            value = null;
-        }
-        return value;
-
-    }
 }
-
--- src/Searcher.java
+++ src/Searcher.java
@@ -1,34 +1,19 @@
+/**
+ * Created by Zohreh on 08/20/2015.
+ */
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.*;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.search.ScoreDoc;
-
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Date;
 
 
 public class Searcher {
-
     public static void main(String[] args) throws IOException, ParseException {
         long start = new Date().getTime();
 
@@ -39,92 +24,29 @@ public class Searcher {
         long end = new Date().getTime();
         System.out.println("Searching took " + (end - start) + " milliseconds");
     }
-
     public Searcher(String indexDir, String q)  {
         try {
             IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexDir)));
-            IndexSearcher searcher = new IndexSearcher(reader);
             Analyzer analyzer = new StandardAnalyzer();
 
-            String queries="";
-            String field="Score";
-
-            QueryParser parser = new QueryParser(field, analyzer);
-
-            Query query = parser.parse(q);
-            System.out.println("Searching for: " + query.toString(field));
-
-            searcher.search(query, 100);
-
-            // Collect enough docs to show 5 pages
-            TopDocs results = searcher.search(query, 10);
-            ScoreDoc[] hits = results.scoreDocs;
-
-            int numTotalHits = results.totalHits;
-            System.out.println(numTotalHits + " total matching documents");
-
-
-            int start = 0;
-            int end = Math.min(numTotalHits, 10);
-
-            for (int i = start; i < end; i++) {
-                System.out.println("doc="+hits[i].doc+" score="+hits[i].score);
-
-                Document doc = searcher.doc(hits[i].doc);
-            }
-
-            reader.close();
-
-            IndexReader reader2 = DirectoryReader.open(FSDirectory.open(Paths.get(indexDir)));
             int c=0;
-            for (int i=0; i<reader2.maxDoc(); i++) {
-                Document doc = reader2.document(i);
-                String docId = doc.get("Id");
-                String Score = doc.get("Score");
-                String Title = doc.get("Title");
-                //System.out.println("Id="+docId+" \'nScore="+Score+" Title="+Title);
-
+            for (int i=0; i<reader.maxDoc(); i++) {
+                Document doc = reader.document(i);
+                String output="";
+                System.out.println("Id="+doc.get("Id")+" PostTypeId="+doc.get("PostTypeId")+" ParentId="+doc.get("ParentId")+
+                        " AcceptedAnswerId="+doc.get("AcceptedAnswerId")+" CreationDate="+doc.get("CreationDate")+" Score="+
+                        doc.get("Score")+" ViewCount="+doc.get("ViewCount")+" Body="+doc.get("Body")+" OwnerUserId="+doc.get("OwnerUserId")+
+                                " LastEditorUserId="+doc.get("LastEditorUserId")+" LastEditorDisplayName="+doc.get("LastEditorDisplayName")+
+                        " LastEditDate="+doc.get("LastEditDate")+" LastActivityDate="+doc.get("LastActivityDate")+" Title="+doc.get("Title")+
+                        " Tags="+doc.get("Tags")+" AnswerCount="+doc.get("AnswerCount")+" CommentCount="+doc.get("CommentCount")+ " FavoriteCount="+
+                        doc.get("FavoriteCount")+ " CommunityOwnedDate="+doc.get("CommunityOwnedDate"));
                 c=c+1;
             }
             System.out.println("TotalNum="+c);
 
-            reader2.close();
-            //Directory indexDir = FSDirectory.open(".\\index");
-            //IndexReader rdr = DirectoryReader.open(FSDirectory.open(new File(indexDir)));
-            //IndexSearcher is = new IndexSearcher(rdr);
-
-        } catch (IOException | ParseException e) {
+            reader.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    /*Analyzer analyzer = new StandardAnalyzer();
-
-    // Store the index in memory:
-    Directory directory = new RAMDirectory();
-    // To store an index on disk, use this instead:
-    //Directory directory = FSDirectory.open("/tmp/testindex");
-    IndexWriterConfig config = new IndexWriterConfig(analyzer);
-    IndexWriter iwriter = new IndexWriter(directory, config);
-    Document doc = new Document();
-    String text = "This is the text to be indexed.";
-    doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
-    iwriter.addDocument(doc);
-    iwriter.close();
-
-    // Now search the index:
-    DirectoryReader ireader = DirectoryReader.open(directory);
-    IndexSearcher isearcher = new IndexSearcher(ireader);
-    // Parse a simple query that searches for "text":
-    QueryParser parser = new QueryParser("fieldname", analyzer);
-    Query query = parser.parse("text");
-    ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
-    assertEquals(1, hits.length);
-    // Iterate through the results:
-    for (int i = 0; i < hits.length; i++) {
-        Document hitDoc = isearcher.doc(hits[i].doc);
-        assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
-    }
-    ireader.close();
-    directory.close();*/
 }
